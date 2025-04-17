6c831cb822cba21f7fe9c5860716b937e38b218d
Pedro Almeida <phalmeidasantos@gmail.com>
2025-04-17 00:26:01 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
+using System;
+
+namespace GeradorDeSenhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao gerador de senha segura\n");
+
+            Console.Write("Informe quantos caracteres terá a senha (mínimo 4): ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            GeradorSenha gerador = new GeradorSenha();
+
+            try
+            {
+                string senha = gerador.GerarSenha(tamanho);
+                Console.WriteLine($"\nSenha gerada: {senha}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
 
